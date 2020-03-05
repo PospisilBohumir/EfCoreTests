4a065d3fe83620a87371dd848b448e68912d8ff1
pospisilb <pospisilb@cngroup.dk>
2020-03-05 23:21:48 +0100

    Add project files.

---
+++ EfCoreTests/Migrations/20200305173620_Initial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EfCoreTests.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TestEntities",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TestOwnedEntity_Code = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestEntities", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TestEntities");
+        }
+    }
+}
---
+++ EfCoreTests/OwnedEntityProxyTests.cs
@@ -0,0 +1,98 @@
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace EfCoreTests
+{
+    public class OwnedEntityProxyTests
+    {
+        [Fact]
+        public void InMemoryTest()
+        {
+            Assert.True(Test(new DbContextOptionsBuilder<TestContext>()
+                .UseInMemoryDatabase(databaseName: "Test")));
+        }
+
+        [Fact]
+        public void MsSqlTest()
+        {
+            Assert.True(Test(new DbContextOptionsBuilder<TestContext>()
+                .UseSqlServer("Data Source=.;MultipleActiveResultSets=True;Integrated Security=True")));
+        }
+
+        private static bool Test(DbContextOptionsBuilder<TestContext> builder)
+        {
+            var options = builder.UseLazyLoadingProxies().Options;
+            using (var ctx = new TestContext(options))
+            {
+                ctx.TestEntities.Add(new TestEntity());
+                ctx.SaveChanges();
+            }
+
+            using (var ctx = new TestContext(options))
+            {
+                var e = ctx.TestEntities.Single();
+                e.TestOwnedEntity.Code = "test";
+                return ctx.ChangeTracker.Entries().Any(o => o.State != EntityState.Unchanged);
+            }
+        }
+    }
+
+    public class TestContext : DbContext
+    {
+        public TestContext() : base(new DbContextOptionsBuilder<TestContext>()
+            .UseSqlServer("Data Source=.;MultipleActiveResultSets=True;Integrated Security=True")
+            .UseLazyLoadingProxies()
+            .Options)
+        {
+        }
+
+        public TestContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<TestEntity> TestEntities { get; set; }
+    }
+
+    public class TestEntity
+    {
+        public long Id { get; set; }
+        public virtual TestOwnedEntity TestOwnedEntity { get; private set; } = new TestOwnedEntity();
+    }
+
+    [Owned]
+    public class TestOwnedEntity
+    {
+        public string Code { get; set; }
+
+        protected bool Equals(TestOwnedEntity other)
+        {
+            return string.Equals(Code, other.Code);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return Equals((TestOwnedEntity) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Code != null ? Code.GetHashCode() : 0);
+        }
+    }
+}
